06ac2026582f326df520afe7c121b553994940d2
Aviroop Jana <aviroopjana@gmail.com>
2023-05-19 23:48:15 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import express, { Express, Response, Request } from "express";
 import Moralis from "moralis";
 import dotenv from "dotenv";
 import cors from "cors";
-const app = express();
+const app: Express = express();
 const port = 5001;
 dotenv.config();
 
@@ -11,4 +11,16 @@ app.use(express.json());
 
 const API_KEY = process.env.MORALIS_API_KEY; 
 
-// Our EndPoint 
+// startServer function to initialise Moralis
+
+const startServer = async () => {
+    await Moralis.start({
+        apiKey: API_KEY,
+    }).then(() => {
+        app.listen(port, () => {
+            console.log("Moralis Server started successfully!")
+        });
+    });
+};
+
+startServer();
