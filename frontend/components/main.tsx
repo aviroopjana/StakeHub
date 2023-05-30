cdf0c7d56c075520b9fc626a38bb7d455705bc84
Aviroop Jana <aviroopjana@gmail.com>
2023-05-30 16:32:04 +0530

    added main.tsx

---
+++ frontend/components/main.tsx
@@ -0,0 +1,10 @@
+import styles from "../styles/Home.module.css"
+
+export default function main(): JSX.Element {
+    return (
+        <section className="styles.container" >
+            {/* <staking/> */}
+            {/* <stakingData/> */}
+        </section>
+    )
+}
